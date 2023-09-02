54bd2733c568325234e2d3ab536656ec8ddf8a7f
Luca Moschella <luca.moschella94@gmail.com>
2023-09-02 13:21:08 +0200

    Add dynamic transpose conv2d

--- tests/nn/test_dyncnn.py
+++ tests/nn/test_dyncnn.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from anypy.nn.dynccn import infer_convolution
+from anypy.nn.dynccn import infer_convolution2d, infer_transposed_convolution2d
 
 
 @pytest.mark.parametrize(
@@ -38,7 +38,7 @@ def test_infer_convolution(
 ):
     x = torch.randn(input_shape)
 
-    conv = infer_convolution(
+    conv = infer_convolution2d(
         input_shape,
         expected_output_shape,
         kernel_size=kernel_size,
@@ -46,7 +46,6 @@ def test_infer_convolution(
         padding=padding,
         dilation=dilation,
     )
-    print(conv)
 
     y = conv(x)
 
@@ -73,11 +72,97 @@ def test_infer_convolution_errors(
     dilation,
 ):
     with pytest.raises(ValueError):
-        _ = infer_convolution(
+        _ = infer_convolution2d(
             input_shape,
             expected_output_shape,
             kernel_size=kernel_size,
             stride=stride,
             padding=padding,
+            dilation=dilation,
+        )
+
+
+@pytest.mark.parametrize(
+    "input_shape, expected_output_shape, kernel_size, stride, padding, output_padding, dilation",
+    [
+        # kernel
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, 1, 0, (0, 0), (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 37, 37), None, 1, 0, (0, 0), (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 42, 42), None, 1, 0, (0, 0), (1, 1)),
+        # stride
+        ((1, 3, 32, 32), (1, 16, 65, 65), (3, 3), None, 0, (0, 0), (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 96, 96), (3, 3), None, 0, (0, 0), (1, 1)),
+        # padding
+        ((1, 3, 32, 32), (1, 16, 34, 34), (3, 3), (1, 1), None, (0, 0), (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 61, 61), (3, 3), (2, 2), None, (0, 0), (1, 1)),
+        # output padding
+        ((1, 3, 32, 32), (1, 16, 66, 66), (3, 3), (2, 2), 0, None, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 65, 65), (3, 3), (2, 2), 0, None, (1, 1)),
+        # dilation
+        ((1, 3, 32, 32), (1, 16, 65, 65), (3, 3), (2, 2), 0, 0, None),
+        ((1, 3, 32, 32), (1, 16, 67, 67), (3, 3), (2, 2), 0, 0, None),
+        ((1, 3, 32, 32), (1, 16, 73, 73), (3, 3), (2, 2), 0, 0, None),
+    ],
+)
+def test_infer_transposed_convolution(
+    input_shape,
+    expected_output_shape,
+    kernel_size,
+    stride,
+    padding,
+    output_padding,
+    dilation,
+):
+    x = torch.randn(input_shape)
+
+    conv = infer_transposed_convolution2d(
+        input_shape,
+        expected_output_shape,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        output_padding=output_padding,
+        dilation=dilation,
+    )
+
+    y = conv(x)
+
+    assert y.shape == expected_output_shape
+
+
+@pytest.mark.parametrize(
+    "input_shape, expected_output_shape, kernel_size, stride, padding, output_padding, dilation",
+    [
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, None, (1, 1), None, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 32, 32), (3, 3), None, None, None, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 32, 32), (3, 3), (1, 1), None, None, None),
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, (1, 1), None, None, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, (1, 1), (1, 1), None, None),
+        ((1, 3, 32, 32), (1, 16, 32, 32), (3, 3), None, (1, 1), None, None),
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, None, (1, 1), 0, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 32, 32), (3, 3), None, None, 0, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 32, 32), (3, 3), (1, 1), None, 0, None),
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, (1, 1), None, 0, (1, 1)),
+        ((1, 3, 32, 32), (1, 16, 32, 32), None, (1, 1), (1, 1), 0, None),
+        ((1, 3, 32, 32), (1, 16, 32, 32), (3, 3), None, (1, 1), 0, None),
+    ],
+)
+def test_infer_tranpose_convolution_errors(
+    input_shape,
+    expected_output_shape,
+    kernel_size,
+    stride,
+    padding,
+    output_padding,
+    dilation,
+):
+    with pytest.raises(ValueError):
+        _ = infer_transposed_convolution2d(
+            input_shape,
+            expected_output_shape,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            output_padding=output_padding,
             dilation=dilation,
         )
