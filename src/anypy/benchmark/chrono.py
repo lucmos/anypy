860450fc951bd9259d5a106edbb0bfb1daa48d0a
Luca Moschella <luca.moschella94@gmail.com>
2023-09-11 13:46:13 +0200

    Add documentation

--- src/anypy/benchmark/chrono.py
+++ src/anypy/benchmark/chrono.py
@@ -11,7 +11,7 @@ class ChronoAlreadyStoppedError(ChronoRuntimeError):
 
 
 class ChronoAlreadyStartedError(ChronoRuntimeError):
-    """raised when trying to start a started timer."""
+    """Raised when trying to start a started timer."""
 
 
 class Chrono:
@@ -29,6 +29,7 @@ class Chrono:
         precision: int,
         _is_direct: bool = True,
     ):
+        """Chrono constructor (do not use directly)."""
         if _is_direct:
             raise ChronoRuntimeError("Do not initialize Chrono directly, use Chrono.start(name).")
         self.precision = precision
@@ -36,12 +37,19 @@ class Chrono:
         self.name = name
 
     def _current_prefix() -> str:
+        """Return the current prefix for the timer according to the number of active timers."""
         return Chrono.PREFIX * len(Chrono.active_timers)
 
     def clock(self) -> float:
+        """Return the elapsed time since the timer started.
+
+        Returns:
+            float: The elapsed time.
+        """
         return time() - self._start
 
     def __enter__(self):
+        """Start the timer with a context manager."""
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -49,7 +57,18 @@ class Chrono:
 
     @staticmethod
     def start(name: str, precision: int = 5):
-        """Start the timer."""
+        """Start the timer.
+
+        Args:
+            name (str): The name of the timer.
+            precision (int, optional): The precision of the timer. Defaults to 5.
+
+        Returns:
+            Chrono: The Chrono object.
+
+        Raises:
+            ChronoAlreadyStartedError: If the timer is already started.
+        """
         if name in Chrono.active_timers:
             raise ChronoAlreadyStartedError("Chrono already started.")
         current_prefixes = Chrono._current_prefix()
@@ -58,7 +77,11 @@ class Chrono:
         return Chrono.active_timers[name]
 
     def stop(self):
-        """Stop the timer."""
+        """Stop the timer.
+
+        Raises:
+            ChronoAlreadyStoppedError: If the timer is already stopped.
+        """
         if self.name not in Chrono.active_timers:
             raise ChronoAlreadyStoppedError("Chrono already stopped.")
         del Chrono.active_timers[self.name]
@@ -72,7 +95,11 @@ class Chrono:
         )
 
     def elapsed(self) -> str:
-        """Get the elapsed time."""
+        """Lap time.
+
+        Raises:
+            ChronoAlreadyStoppedError: If the timer is already stopped.
+        """
         if self.name not in Chrono.active_timers:
             raise ChronoAlreadyStoppedError("Chrono already stopped.")
         print(
