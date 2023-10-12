188092c10031fe47e190161d949888cd8f3cc6e8
Luca Moschella <luca.moschella94@gmail.com>
2023-10-12 11:32:56 +0200

    Add function to compute adaptive loss weights

--- src/anypy/nn/utils.py
+++ src/anypy/nn/utils.py
@@ -1,7 +1,7 @@
 import logging
 
 import torch
-from torch import nn
+from torch import Tensor, nn
 
 pylogger = logging.getLogger(__name__)
 
@@ -25,3 +25,30 @@ def infer_dimension(width: int, height: int, n_channels: int, model: nn.Module, 
         fake_out = model(fake_batch)
         model.train()
         return fake_out
+
+
+def calculate_adaptive_weight(loss_a: Tensor, loss_b: Tensor, layer: Tensor) -> float:
+    """Compute the re-scaling factor to apply to loss_b.
+
+    The rescaling factor is computed such taht the gradients of both the loss terms
+    wrt the specified layer have the same norm.
+
+    Args:
+        loss_a: first loss term
+        loss_b: second loss term
+        layer: compute the gradients of loss_a and loss_b with respect to this tensor
+
+    Returns:
+        the scaling factor to apply to loss_b,
+        in order to have the same gradient norm of loss_a in the layer
+    """
+    try:
+        loss_a_grads = torch.autograd.grad(loss_a, layer, retain_graph=True)[0]
+        loss_b_grads = torch.autograd.grad(loss_b, layer, retain_graph=True)[0]
+
+        loss_b_weight = torch.norm(loss_a_grads) / (torch.norm(loss_b_grads) + 1e-4)
+        loss_b_weight = torch.clamp(loss_b_weight, 0.0, 1e6).detach()
+    except RuntimeError:
+        pylogger.error("Error in calculating adaptive weight")
+        loss_b_weight = torch.tensor(0.0)
+    return loss_b_weight
