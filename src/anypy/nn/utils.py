829687e942d168e0d979b1121ff54dcec25a174a
Luca Moschella <luca.moschella94@gmail.com>
2023-10-12 11:52:04 +0200

    Parameterize function

--- src/anypy/nn/utils.py
+++ src/anypy/nn/utils.py
@@ -27,7 +27,15 @@ def infer_dimension(width: int, height: int, n_channels: int, model: nn.Module, 
         return fake_out
 
 
-def calculate_adaptive_weight(loss_a: Tensor, loss_b: Tensor, layer: Tensor) -> float:
+def calculate_adaptive_weight(
+    loss_a: Tensor,
+    loss_b: Tensor,
+    layer: Tensor,
+    min: float = 0.0,
+    max: float = 1e6,
+    eps: float = 1e-4,
+    fallback: float = 0.0,
+) -> float:
     """Compute the re-scaling factor to apply to loss_b.
 
     The rescaling factor is computed such taht the gradients of both the loss terms
@@ -37,6 +45,10 @@ def calculate_adaptive_weight(loss_a: Tensor, loss_b: Tensor, layer: Tensor) -> 
         loss_a: first loss term
         loss_b: second loss term
         layer: compute the gradients of loss_a and loss_b with respect to this tensor
+        min: minimum value of the scaling factor
+        max: maximum value of the scaling factor
+        eps: epsilon value to avoid division by zero
+        fallback: value to return in case of autograd error
 
     Returns:
         the scaling factor to apply to loss_b,
@@ -46,9 +58,9 @@ def calculate_adaptive_weight(loss_a: Tensor, loss_b: Tensor, layer: Tensor) -> 
         loss_a_grads = torch.autograd.grad(loss_a, layer, retain_graph=True)[0]
         loss_b_grads = torch.autograd.grad(loss_b, layer, retain_graph=True)[0]
 
-        loss_b_weight = torch.norm(loss_a_grads) / (torch.norm(loss_b_grads) + 1e-4)
-        loss_b_weight = torch.clamp(loss_b_weight, 0.0, 1e6).detach()
+        loss_b_weight = torch.norm(loss_a_grads) / (torch.norm(loss_b_grads) + eps)
+        loss_b_weight = torch.clamp(loss_b_weight, min=min, max=max).detach()
     except RuntimeError:
         pylogger.error("Error in calculating adaptive weight")
-        loss_b_weight = torch.tensor(0.0)
+        loss_b_weight = torch.tensor(fallback)
     return loss_b_weight
